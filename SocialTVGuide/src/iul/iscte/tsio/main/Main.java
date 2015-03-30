fa14b1661ffc4b89a19698cd7e5a5dac1db17a46
DiogoPedroso <gotmo666@gmail.com>
2015-03-30 16:42:32 +0100

    - Rebase

--- SocialTVGuide/src/iul/iscte/tsio/main/Main.java
+++ SocialTVGuide/src/iul/iscte/tsio/main/Main.java
@@ -1,58 +1,10 @@
 package iul.iscte.tsio.main;
 
-import javax.swing.JOptionPane;
-
-import iul.iscte.tsio.server.Server;
-import iul.iscte.tsio.view.UsersView;
-import iul.iscte.tsio.view.inputPanes.ServerAddressRequestToUserPane;
+import iul.iscte.tsio.view.LoginView;
 
 public class Main {
 
-	public static void main(String[] args) {
-		// System.out.println("Enter the server address: ");
-		// Scanner scanner = new Scanner(System.in);
-		// String serverAddress = scanner.nextLine();
-		// scanner.close();
-
-		boolean connected = false;
-		boolean authenticated = false;
-
-		while (!connected && !authenticated) {
-			// Get Server IP Address/ Hostname
-			ServerAddressRequestToUserPane addressRequestPane = new ServerAddressRequestToUserPane();
-			String serverAddress = "http://"
-					+ addressRequestPane.getIpAddressAsString() + ":"
-					+ addressRequestPane.getPortNumberAsString() + "/db/data/";
-
-			System.out.println("Trying to connect to server \"" + serverAddress
-					+ "\"...");
-
-			try {
-				if (!Server.getInstance().isConnectedToServer()) {
-					Server.getInstance().login(serverAddress);
-				}
-				connected = Server.getInstance().isConnectedToServer();
-			} catch (com.sun.jersey.api.client.ClientHandlerException e) {
-				JOptionPane.showMessageDialog(null,
-						"Connection Timed Out. Please check address and port!",
-						"Connection Error", JOptionPane.ERROR_MESSAGE);
-			}
-
-			authenticated = Server.getInstance().setLoggedUser(
-					"techsupport@lemonparty.com");
-
-			// Create GUI
-			if (authenticated) {
-				UsersView.getInstance().setVisible(true);
-				JOptionPane.showMessageDialog(null, "Successfully Connected!",
-						"Connection Information", JOptionPane.INFORMATION_MESSAGE);
-			} else if(connected){
-				//System.out.println("It was not possible to authenticate user");
-				JOptionPane.showMessageDialog(null,
-						"Unable to autenticate user. Please correct username!",
-						"Authentication Error", JOptionPane.ERROR_MESSAGE);
-				// System.exit(0);
-			}
-		}
+	public static void main(String[] args) {		
+		new LoginView();
 	}
 }
